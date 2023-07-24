5c5619f2275686b1395d5b6b79f2d441cacf59be
talemwaraymond <talemwaray@gmail.com>
2023-07-24 10:54:33 -0700

    3-main.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+        while (str[i])
+        {
+              _putchar(str[i]);
+	      i++;
+        }
+         _putchar('\n');
+}
