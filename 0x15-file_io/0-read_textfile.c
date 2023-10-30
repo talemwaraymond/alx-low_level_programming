62b431121eaaf30312b5973153faae6e94cbe4c9
talemwaraymond <talemwaray@gmail.com>
2023-10-30 07:11:15 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,14 +8,12 @@
  * Return: w- actual number of bytes read and printed
  * 0 when function fails or filename is NULL
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
-ssize_t fd; 
+ssize_t fd;
 ssize_t w;
 ssize_t t;
-
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
