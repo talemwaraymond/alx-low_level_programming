eb09b3230e03862e2ec36c1195f415b18abd5f9a
talemwaraymond <talemwaray@gmail.com>
2024-04-11 13:36:13 -0700

    linear

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,51 +1,61 @@
 #include "search_algos.h"
 
 /**
- * print_array - print an array
- * @array: the array to print
- * @l: the left index
- * @r: the right index
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-void print_array(int *array, int l, int r)
+int recursive_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t half = size / 2;
+	size_t i;
 
-	for (i = l; i <= r; i++)
-	{
-		printf("%d", array[i]);
-		if (i < r)
-			printf(", ");
-	}
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
 	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
- * binary_search - search for an element in an array
- * @array: the array we want to search in
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
  * @size: size of the array
- * @value: the target value
- * Return: the index of the value or -1 if not found
+ * @value: value to search in
+ * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int l = 0, r = size - 1, m;
+	int index;
 
-	if (!array || size == 0)
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
 
-	while (l <= r)
-	{
-		printf("Searching in array: ");
-		print_array(array, l, r);
-		m = l + (r - l) / 2;
-
-		if (array[m] > value)
-			r = m - 1;
-		else if (array[m] < value)
-			l = m + 1;
-		else
-			return (m);
-	}
-
-	return (-1);
+	return (index);
 }
